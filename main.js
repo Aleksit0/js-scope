90e984310471681f9b38b8f26ee2d381862c8b95
Aleksit0 <50078646+Aleksit0@users.noreply.github.com>
2020-08-31 18:36:39 +0200

    Js scope examples

---
+++ main.js
@@ -0,0 +1,17 @@
+// In Javascript there are 2 types of scopes:
+// GLOBAL and LOCAL scope
+
+// GLOABL SCOPE
+let name = "global Octocat"
+console.log(name);
+
+// LOCAL SCOPE - example no.1
+function getName() {
+  let name = 'local Octocat';
+    /* If there is no name variable in this function,
+      the program will search for that same variable
+      but outside this function.  */ 
+  console.log(name);
+}
+
+getName();
